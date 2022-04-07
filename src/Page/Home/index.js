ae44b94c5f15e931e6cb5e21bb55a9593d9d5470
faishallj <fjanuarahman@gmail.com>
2022-04-08 00:18:48 +0700

    adding root page and CreatePlaylist page with react router

--- src/Page/Home/index.js
+++ src/Page/Home/index.js
@@ -4,28 +4,29 @@ import SongInfo from "../../Component/SongInfo";
 import Search from "../../Component/SearchBar";
 import Playlist from "../../Component/FormPlaylist";
 import config from "../../lib/config";
-import { ToastContainer, toast } from "react-toastify";
+import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css"
 import { getProfile } from "../../lib/spotifyConfig";
-import {useDispatch, useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { login } from "../../accountSlice";
+import { BrowserRouter, Redirect, Switch, Route, Link } from "react-router-dom";
 
 
 const Home = () => {
-   
+
     const [tracks, setTracks] = useState([]);
     const [selectedTrackUri, setSelectedTrackUri] = useState([]);
     const [selectedTrack, setSelectedTrack] = useState([]);
 
-    const auth = useSelector((state)=> state.auth.login)
-    const dispatch = useDispatch()
+    const isLogin = useSelector((state) => state.auth.login);
+    const dispatch = useDispatch();
 
 
     useEffect(() => {
         const access_token = new URLSearchParams(window.location.hash).get('#access_token');
 
         if (access_token !== null) {
-            const userProfile = async ()=> {
+            const userProfile = async () => {
                 try {
                     const response = await getProfile(access_token);
                     dispatch(
@@ -34,16 +35,16 @@ const Home = () => {
                             user: response,
                         })
                     )
-                } catch(e) {
+                } catch (e) {
                     toast.error(e);
-                } 
+                }
             }
 
             userProfile();
-        } 
+        }
     }, []);
 
-    const authorization = ()=> {
+    const authorization = () => {
         return `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_SPOTIFY_KEY}&redirect_uri=http://localhost:3000&response_type=token&scope=${config.SPOTIFY_SCOPE}`
     }
     const searchResultSuccess = (data) => {
@@ -51,7 +52,7 @@ const Home = () => {
             (track) => selectedTrackUri.includes(track.uri)
         );
 
-       setTracks([...new Set([...selectedTracks, ...data])]);
+        setTracks([...new Set([...selectedTracks, ...data])]);
     }
 
     const toggleSelect = (track) => {
@@ -59,7 +60,7 @@ const Home = () => {
 
         if (selectedTrackUri.includes(uri)) {
             setSelectedTrackUri(selectedTrackUri.filter((item) => item !== uri));
-            setSelectedTrack(selectedTrack.filter((item)=> item.uri !== uri))
+            setSelectedTrack(selectedTrack.filter((item) => item.uri !== uri))
 
         } else {
             setSelectedTrackUri([...selectedTrackUri, uri]);
@@ -80,48 +81,56 @@ const Home = () => {
 
     return (
         <div>
-            <ToastContainer />
-            {!auth && (
-                <div className="login-container">
-                    <h2>Please Login</h2>
-                    <a href={authorization()}>Login</a>
-                </div>
-            )}
-
-            {auth && (
-                <div className="home-container">
-                    <div className="button-container">
-                        <a onClick={logOut}>Logout</a>
-                        <h3>Search Track Here</h3>
-                        <div>
-                            <Search searchResult={(tracks) => searchResultSuccess(tracks)} />
-                        </div>
-                        {tracks.length > 0 && (
-                            <div className="playlist-container">
-                                <h2>Create Playlist</h2>
-                                <Playlist uri={selectedTrackUri} />
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/">
+                        {!isLogin && (
+                            <div className="login-container">
+                                <h2>Please Login</h2>
+                                <a href={authorization()}>Login</a>
                             </div>
                         )}
 
-                    </div>
-                    <div className="container" >
-                        {tracks.map(e => (
-                            <div className="wrapper">
-                                <div className="title-container">
-                                    <SongInfo 
-                                    key={e.uri} 
-                                    url={e.album.images[1].url} 
-                                    title={e.name} 
-                                    artist={e.artists[0].name} 
-                                    toggleSelect={() => toggleSelect(e)}
-                                    select = {selectedTrackUri.includes(e.uri)} 
-                                    />
+                        {isLogin && (
+                            <Redirect to="/CreatePlaylist"></Redirect>
+                        )}
+                    </Route>
+                    <Route path="/CreatePlaylist">
+                        <div className="home-container">
+                            <div className="button-container">
+                                <a onClick={logOut}>Logout</a>
+                                <h3>Search Track Here</h3>
+                                <div>
+                                    <Search searchResult={(tracks) => searchResultSuccess(tracks)} />
                                 </div>
+                                {tracks.length > 0 && (
+                                    <div className="playlist-container">
+                                        <h2>Create Playlist</h2>
+                                        <Playlist uri={selectedTrackUri} />
+                                    </div>
+                                )}
+
                             </div>
-                        ))}
-                    </div>
-                </div>
-            )}
+                            <div className="container" >
+                                {tracks.map(e => (
+                                    <div className="wrapper">
+                                        <div className="title-container">
+                                            <SongInfo
+                                                key={e.uri}
+                                                url={e.album.images[1].url}
+                                                title={e.name}
+                                                artist={e.artists[0].name}
+                                                toggleSelect={() => toggleSelect(e)}
+                                                select={selectedTrackUri.includes(e.uri)}
+                                            />
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </Route>
+                </Switch>
+            </BrowserRouter>
         </div>
     )
 }
